575e6f07ae3265f8966eeb05e71b5382c37d16c5
Reyezonia <reyezonia@gmail.com>
2022-10-22 17:17:02 -0500

    Update database.js

--- back/config/database.js
+++ back/config/database.js
@@ -1,12 +1,14 @@
 //conectar base de datos
 const mongoose=require("mongoose");
 
-const connectDatabase =()=>{
+const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    }).then(con =>{
+    }).then(con => {
         console.log(`Base de datos mongo conectada con el servidor: ${con.connection.host}`)
+    }).catch(con => {
+        console.log(`No se logro la conexion con la base de datos`)
     })
 }
 
