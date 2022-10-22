1cddb76794fbffaafa92d7fb96338f40cf3a8199
Reyezonia <reyezonia@gmail.com>
2022-10-22 17:16:07 -0500

    ventas controlador y ruta

--- back/app.js
+++ back/app.js
@@ -1,16 +1,16 @@
-const express=require("express");
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 //importar rutas
-const productos=require("./routes/products")
+const productos= require("./routes/products")
 
 app.use('/api',productos) //debatir donde va la lista de productos
 
-module.exports = app
-
-//aqui se registra lo que ya esta listo
-const productos=require("./routes/ventas")
+/**aqui se registra lo que ya esta listo*/
+const ventas = require('./routes/ventas')
+app.use('/api', ventas)
 //modelo-controlador-apps 
-//ruta de navegador app.use('/api',index) home 
+/**ruta de navegador app.use('/api',index) home */
+module.exports = app
--- back/routes/ventas.js
+++ back/routes/ventas.js
@@ -1,12 +1,17 @@
-const express = require("express")
+const express = require('express')
 //importar
 const router = express.Router();
 //se crea un enrutador
-const{getVentas} = require("../controllers/ventasControllers")
-//listar ventas servicio
+//const{getVenta} = require('../controllers/ventasControllers')
+const {getVenta, newVenta, getProductById, updateVenta, deleteventa} = require("../controllers/ventasController") 
+//Traemos la respuesta json desde el controlador
+//listar ventas servicio y demas
 
-router.route('/ventas').get(getVentas)
-//se traza ruta para ver la lista de ventas
-module.exports = router;
-//mostrar o exportar
+router.route('/ventas').get(getVenta)//se traza ruta para ver la lista de ventas
+router.route('/venta/nuevo').post(newVenta);//ruta para crear
+router.route('/venta/:id').get(getProductById);//ruta para buscar
+router.route('/venta/:id').put(updateVenta);//rurta para actualizar
+router.route('/venta/:id').delete(deleteventa);//ruta para eliminar
+
+module.exports = router; //mostrar o exportar
 
